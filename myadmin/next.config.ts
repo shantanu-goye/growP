a33851b41435f689790f93feb6b6753e8c4f3729
lokidass <chaudharylokesh53284@gmail.com>
2025-05-27 16:02:14 +0530

    eslintconfig

--- myadmin/next.config.ts
+++ myadmin/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // This option disables ESLint during `next build`
+    ignoreDuringBuilds: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
