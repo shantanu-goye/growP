f71ebe43deabdb8c70f0718dddc24f82a5a15bd0
Shantanu Gote <shantanugote82@gmail.com>
2025-06-15 13:02:03 +0530

    refactor: update CORS middleware to include methods and allowed headers

--- src/app.js
+++ src/app.js
@@ -27,6 +27,8 @@ app.use(
       "https://admin.growp.in",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
